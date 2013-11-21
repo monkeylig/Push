e58b11f38420512db4d923fd11db476a864cc1ff
monkeylig <jhard9000@gmail.com>
2013-11-21 16:46:40 -0500

    First

---
+++ Test/platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\PhoneGap\Test\platforms\android\gen\com\example\test\R.java \
+ : C:\PhoneGap\Test\platforms\android\res\drawable\icon.png \
+C:\PhoneGap\Test\platforms\android\res\drawable-hdpi\icon.png \
+C:\PhoneGap\Test\platforms\android\res\drawable-ldpi\icon.png \
+C:\PhoneGap\Test\platforms\android\res\drawable-mdpi\icon.png \
+C:\PhoneGap\Test\platforms\android\res\drawable-xhdpi\icon.png \
+C:\PhoneGap\Test\platforms\android\res\values\strings.xml \
+C:\PhoneGap\Test\platforms\android\res\xml\config.xml \
+C:\PhoneGap\Test\platforms\android\bin\AndroidManifest.xml \
